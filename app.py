3f0891ade6bb0f37dfa57b21636a29da827d103a
eric-tang-dev <tang695@purdue.edu>
2026-04-20 04:40:00 -0400

    pushing more videos to grid

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from flask import Flask, render_template, request, jsonify, send_from_directory, session, redirect, url_for
 from google.cloud import storage
 from google.api_core.exceptions import NotFound
+from video_index import KALTURA_VIDEOS
 
 
 app = Flask(__name__)   # start instance of Flask
@@ -22,149 +23,9 @@ results_directory.mkdir(parents=True, exist_ok=True)
 
 VALID_EXPERT_IDS = {"1183456", "2234567", "3345678"}
 
-
 # -----------------------------
-# KALTURA VIDEO INDEX
+# STEP OPTIONS FOR DROPDOWN
 # -----------------------------
-KALTURA_VIDEOS = [
-    {
-        "category": "ostomy-skills",
-        "video_name": "S1_compressed",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57908202,
-        "entry_id": "1_pc9nnlfe"
-    },
-    {
-        "category": "ostomy-skills",
-        "video_name": "S3_compressed",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_ifhnx26b"
-    },
-    {
-        "category": "ostomy-skills",
-        "video_name": "S5_compressed",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_5k6laa17"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S10_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_0ukvx295"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S11_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_db5y1w1d"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S12_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_0xox0f5v"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S13_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_8b1ax3dm"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S14_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_xv3090hc"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S15_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_bcsjp5p4"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S16_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_3qk65el8"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S17_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_yc3r5qe0"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S18_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_hxpqt5l2"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S19_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_k7f8r6ha"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S20_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_yc3r5qe0"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S21_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_gw7va375"
-    },
-    {
-        "category": "sterile-gloves",
-        "video_name": "S22_compressed_gloves",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_7p7029hd"
-    },
-    {
-        "category": "TEST VIDEO FOR DEVELOPER",
-        "video_name": "test_1",
-        "target_id": "kaltura_player",
-        "wid": "_6489582",
-        "uiconf_id": 57873862,
-        "entry_id": "1_jyzyakxa"
-    },
-    
-]
 
 STEP_OPTIONS_BY_CATEGORY = {
     "ostomy-skills": [
